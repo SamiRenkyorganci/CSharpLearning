ead14bed848b0b98f036ec7e649b94cf821bd70b
Sami Renkyorgancı <samirenkyorganc@gmail.com>
2018-05-24 01:50:39 +0300

    First Commit Degisken Tipi

---
+++ DegiskenTipi/DegiskenTipi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegiskenTipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string value = "Sami Renkyorganci";
+            int number1 = 12;//Değer tipli değişken
+            int number2 = 8;
+            int number3 = number1; //Stack bölgede saklanırlar. 
+
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+            number3 = 44;
+
+            Console.WriteLine("Number1 {0}", number1);
+            Console.WriteLine("Number3 {0}", number3);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
